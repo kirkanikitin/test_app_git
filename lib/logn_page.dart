e8575aac1a96bacf2c15844de81f2165c886ceee
macbok <and.artem.sedov@yandex.ru>
2025-07-14 18:27:25 +0600

    login text appBar commit

--- lib/logn_page.dart
+++ lib/logn_page.dart
@@ -5,7 +5,10 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('gg'),
+      ),
       body: Center(
         child: Column(
           children: [
