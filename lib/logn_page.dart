237b60752308748aaf19d5cee5966e0c4ace7437
macbok <and.artem.sedov@yandex.ru>
2025-07-14 18:21:32 +0600

    login text commit

--- lib/logn_page.dart
+++ lib/logn_page.dart
@@ -5,6 +5,10 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text('ffi'),
+      ),
+    );
   }
 }
